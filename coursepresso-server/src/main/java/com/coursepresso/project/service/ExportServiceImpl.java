75c6a5682e85b9760b51d039ea9e2d3ba2e7a8cc
FoolInTheWave <thefoolinthewave@gmail.com>
2015-04-19 22:50:31 -0400

    Export Working
    Export methods are creating delimited files successfully.

--- coursepresso-server/src/main/java/com/coursepresso/project/service/ExportServiceImpl.java
+++ coursepresso-server/src/main/java/com/coursepresso/project/service/ExportServiceImpl.java
@@ -48,8 +48,100 @@ public class ExportServiceImpl implements ExportService {
   }
 
   @Override
+  public String exportAppliances() {
+    StringBuilder sb = new StringBuilder();
+    String query = "SELECT * FROM appliances";
+
+    try {
+      Connection conn = dataSource.getConnection();
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery(query);
+
+      while (rs.next()) {
+        sb.append(rs.getInt("id"));
+        sb.append(';');
+        sb.append(rs.getString("type"));
+        sb.append(';');
+        sb.append(rs.getString("room_number"));
+        sb.append(';');
+        sb.append(rs.getTimestamp("updated_at"));
+        sb.append('\n');
+      }
+
+      conn.close();
+
+      log.debug("Appliance CSV file created successfully");
+    } catch (Exception e) {
+      log.error("Appliance CSV file creation failed: ", e);
+    }
+
+    return sb.toString();
+  }
+
+  @Override
+  public String exportAuthorities() {
+    StringBuilder sb = new StringBuilder();
+    String query = "SELECT * FROM authorities";
+
+    try {
+      Connection conn = dataSource.getConnection();
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery(query);
+
+      while (rs.next()) {
+        sb.append(rs.getInt("id"));
+        sb.append(';');
+        sb.append(rs.getString("username"));
+        sb.append(';');
+        sb.append(rs.getString("authority"));
+        sb.append(';');
+        sb.append(rs.getTimestamp("updated_at"));
+        sb.append('\n');
+      }
+
+      conn.close();
+
+      log.debug("Authority CSV file created successfully");
+    } catch (Exception e) {
+      log.error("Authority CSV file creation failed: ", e);
+    }
+
+    return sb.toString();
+  }
+
+  @Override
+  public String exportCoursePrerequisites() {
+    StringBuilder sb = new StringBuilder();
+    String query = "SELECT * FROM course_prerequisites";
+
+    try {
+      Connection conn = dataSource.getConnection();
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery(query);
+
+      while (rs.next()) {
+        sb.append(rs.getInt("id"));
+        sb.append(';');
+        sb.append(rs.getString("course_number"));
+        sb.append(';');
+        sb.append(rs.getString("prerequisite"));
+        sb.append(';');
+        sb.append(rs.getTimestamp("updated_at"));
+        sb.append('\n');
+      }
+
+      conn.close();
+
+      log.debug("Course prerequisite CSV file created successfully");
+    } catch (Exception e) {
+      log.error("Course prerequisite CSV file creation failed: ", e);
+    }
+
+    return sb.toString();
+  }
+
+  @Override
   public String exportCourseSections(String term) {
-    String query = "select * from course_sections";
     StringBuilder sb = new StringBuilder();
 
     try {
@@ -92,12 +184,336 @@ public class ExportServiceImpl implements ExportService {
         sb.append(rs.getTimestamp("updated_at"));
         sb.append('\n');
       }
-      
+
       conn.close();
 
       log.debug("Course section CSV file created successfully");
     } catch (Exception e) {
       log.error("Course section CSV file creation failed: ", e);
+    }
+
+    return sb.toString();
+  }
+
+  @Override
+  public String exportCourses() {
+    StringBuilder sb = new StringBuilder();
+    String query = "SELECT * FROM courses";
+
+    try {
+      Connection conn = dataSource.getConnection();
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery(query);
+
+      while (rs.next()) {
+        sb.append(rs.getString("course_number"));
+        sb.append(';');
+        sb.append(rs.getString("department"));
+        sb.append(';');
+        sb.append(rs.getString("title"));
+        sb.append(';');
+        sb.append(rs.getInt("credits"));
+        sb.append(';');
+        sb.append(rs.getString("description"));
+        sb.append(';');
+        sb.append(rs.getString("academic_level"));
+        sb.append(';');
+        sb.append(rs.getTimestamp("updated_at"));
+        sb.append('\n');
+      }
+
+      conn.close();
+
+      log.debug("Course CSV file created successfully");
+    } catch (Exception e) {
+      log.error("Course CSV file creation failed: ", e);
+    }
+
+    return sb.toString();
+  }
+
+  @Override
+  public String exportDepartments() {
+    StringBuilder sb = new StringBuilder();
+    String query = "SELECT * FROM departments";
+
+    try {
+      Connection conn = dataSource.getConnection();
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery(query);
+
+      while (rs.next()) {
+        sb.append(rs.getString("name"));
+        sb.append(';');
+        sb.append(rs.getString("abbreviation"));
+        sb.append(';');
+        sb.append(rs.getTimestamp("updated_at"));
+        sb.append('\n');
+      }
+
+      conn.close();
+
+      log.debug("Department CSV file created successfully");
+    } catch (Exception e) {
+      log.error("Department CSV file creation failed: ", e);
+    }
+
+    return sb.toString();
+  }
+
+  @Override
+  public String exportGroupAuthorities() {
+    StringBuilder sb = new StringBuilder();
+    String query = "SELECT * FROM group_authorities";
+
+    try {
+      Connection conn = dataSource.getConnection();
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery(query);
+
+      while (rs.next()) {
+        sb.append(rs.getLong("group_id"));
+        sb.append(';');
+        sb.append(rs.getString("authority"));
+        sb.append('\n');
+      }
+
+      conn.close();
+
+      log.debug("Group authority CSV file created successfully");
+    } catch (Exception e) {
+      log.error("Group authority CSV file creation failed: ", e);
+    }
+
+    return sb.toString();
+  }
+
+  @Override
+  public String exportGroupMembers() {
+    StringBuilder sb = new StringBuilder();
+    String query = "SELECT * FROM group_members";
+
+    try {
+      Connection conn = dataSource.getConnection();
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery(query);
+
+      while (rs.next()) {
+        sb.append(rs.getLong("id"));
+        sb.append(';');
+        sb.append(rs.getString("username"));
+        sb.append(';');
+        sb.append(rs.getLong("group_id"));
+        sb.append('\n');
+      }
+
+      conn.close();
+
+      log.debug("Group members CSV file created successfully");
+    } catch (Exception e) {
+      log.error("Group members CSV file creation failed: ", e);
+    }
+
+    return sb.toString();
+  }
+
+  @Override
+  public String exportGroups() {
+    StringBuilder sb = new StringBuilder();
+    String query = "SELECT * FROM groups";
+
+    try {
+      Connection conn = dataSource.getConnection();
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery(query);
+
+      while (rs.next()) {
+        sb.append(rs.getLong("id"));
+        sb.append(';');
+        sb.append(rs.getString("group_name"));
+        sb.append('\n');
+      }
+
+      conn.close();
+
+      log.debug("Group CSV file created successfully");
+    } catch (Exception e) {
+      log.error("Group CSV file creation failed: ", e);
+    }
+
+    return sb.toString();
+  }
+
+  @Override
+  public String exportMeetingDays(String term) {
+    StringBuilder sb = new StringBuilder();
+
+    try {
+      Connection conn = dataSource.getConnection();
+      PreparedStatement pstmt = conn.prepareStatement(
+          "SELECT * FROM meeting_days WHERE term like ?"
+      );
+      pstmt.setString(1, term + "%");
+      ResultSet rs = pstmt.executeQuery();
+
+      while (rs.next()) {
+        sb.append(rs.getInt("id"));
+        sb.append(';');
+        sb.append(rs.getInt("course_section_id"));
+        sb.append(';');
+        sb.append(rs.getString("room_number"));
+        sb.append(';');
+        sb.append(rs.getString("day"));
+        sb.append(';');
+        sb.append(rs.getTime("start_time"));
+        sb.append(';');
+        sb.append(rs.getTime("end_time"));
+        sb.append(';');
+        sb.append(rs.getString("term"));
+        sb.append(';');
+        sb.append(rs.getTimestamp("updated_at"));
+        sb.append('\n');
+      }
+
+      conn.close();
+
+      log.debug("Meeting day CSV file created successfully");
+    } catch (Exception e) {
+      log.error("Meeting day CSV file creation failed: ", e);
+    }
+
+    return sb.toString();
+  }
+
+  @Override
+  public String exportProfessors() {
+    StringBuilder sb = new StringBuilder();
+    String query = "SELECT * FROM professors";
+
+    try {
+      Connection conn = dataSource.getConnection();
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery(query);
+
+      while (rs.next()) {
+        sb.append(rs.getInt("id"));
+        sb.append(';');
+        sb.append(rs.getString("first_name"));
+        sb.append(';');
+        sb.append(rs.getString("last_name"));
+        sb.append(';');
+        sb.append(rs.getString("department"));
+        sb.append(';');
+        sb.append(rs.getTimestamp("updated_at"));
+        sb.append('\n');
+      }
+
+      conn.close();
+
+      log.debug("Professor CSV file created successfully");
+    } catch (Exception e) {
+      log.error("Professor CSV file creation failed: ", e);
+    }
+
+    return sb.toString();
+  }
+
+  @Override
+  public String exportRooms() {
+    StringBuilder sb = new StringBuilder();
+    String query = "SELECT * FROM rooms";
+
+    try {
+      Connection conn = dataSource.getConnection();
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery(query);
+
+      while (rs.next()) {
+        sb.append(rs.getString("room_number"));
+        sb.append(';');
+        sb.append(rs.getString("building"));
+        sb.append(';');
+        sb.append(rs.getInt("capacity"));
+        sb.append(';');
+        sb.append(rs.getString("type"));
+        sb.append(';');
+        sb.append(rs.getTimestamp("updated_at"));
+        sb.append('\n');
+      }
+
+      conn.close();
+
+      log.debug("Room CSV file created successfully");
+    } catch (Exception e) {
+      log.error("Room CSV file creation failed: ", e);
+    }
+
+    return sb.toString();
+  }
+
+  @Override
+  public String exportTerms() {
+    StringBuilder sb = new StringBuilder();
+    String query = "SELECT * FROM terms";
+
+    try {
+      Connection conn = dataSource.getConnection();
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery(query);
+
+      while (rs.next()) {
+        sb.append(rs.getString("term"));
+        sb.append(';');
+        sb.append(rs.getString("status"));
+        sb.append(';');
+        sb.append(rs.getTimestamp("updated_at"));
+        sb.append('\n');
+      }
+
+      conn.close();
+
+      log.debug("Term CSV file created successfully");
+    } catch (Exception e) {
+      log.error("Term CSV file creation failed: ", e);
+    }
+
+    return sb.toString();
+  }
+
+  @Override
+  public String exportUsers() {
+    StringBuilder sb = new StringBuilder();
+    String query = "SELECT * FROM users";
+
+    try {
+      Connection conn = dataSource.getConnection();
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery(query);
+
+      while (rs.next()) {
+        sb.append(rs.getString("username"));
+        sb.append(';');
+        sb.append(rs.getString("password"));
+        sb.append(';');
+        sb.append(rs.getString("firstname"));
+        sb.append(';');
+        sb.append(rs.getString("lastname"));
+        sb.append(';');
+        sb.append(rs.getString("email"));
+        sb.append(';');
+        sb.append(rs.getBoolean("enabled"));
+        sb.append(';');
+        sb.append(rs.getString("department"));
+        sb.append(';');
+        sb.append(rs.getTimestamp("updated_at"));
+        sb.append('\n');
+      }
+
+      conn.close();
+
+      log.debug("User CSV file created successfully");
+    } catch (Exception e) {
+      log.error("User CSV file creation failed: ", e);
     }
 
     return sb.toString();
