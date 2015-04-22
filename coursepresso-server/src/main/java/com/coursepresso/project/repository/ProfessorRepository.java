b99a375cd118353c791ab2547593ef8bda8578c9
SteveFoco <sjfoco@svsu.edu>
2015-04-22 12:10:47 -0400

    Ya

--- coursepresso-server/src/main/java/com/coursepresso/project/repository/ProfessorRepository.java
+++ coursepresso-server/src/main/java/com/coursepresso/project/repository/ProfessorRepository.java
@@ -1,6 +1,7 @@
 package com.coursepresso.project.repository;
 
 import com.coursepresso.project.entity.Professor;
+import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -21,5 +22,15 @@ public interface ProfessorRepository extends CrudRepository<Professor, Integer> 
    */
   @Query("SELECT p FROM Professor p WHERE p.id = (:id)")
   public Professor findById(@Param("id") Integer id);
+  
+  /**
+   * Custom FIND method retrieves a CourseSection record from the database with 
+   * an initialized list of MeetingDay records mapped by a foreign key.
+   * 
+   * @return A CourseSection record as a CourseSection object.
+   */
+  @Query("SELECT p FROM Professor p LEFT JOIN FETCH p.courseSectionList")
+  public List<Professor> findAllWithCourseSections();
+  
   
 }
--- coursepresso-server/src/main/java/com/coursepresso/project/service/ConflictServiceImpl.java
+++ coursepresso-server/src/main/java/com/coursepresso/project/service/ConflictServiceImpl.java
@@ -1,9 +1,12 @@
 package com.coursepresso.project.service;
 
 import com.coursepresso.project.entity.Conflict;
+import com.coursepresso.project.entity.CourseSection;
 import com.coursepresso.project.entity.MeetingDay;
+import com.coursepresso.project.entity.Professor;
 import com.coursepresso.project.entity.Room;
 import com.coursepresso.project.entity.Term;
+import com.coursepresso.project.repository.ProfessorRepository;
 import com.coursepresso.project.repository.RoomRepository;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -24,12 +27,17 @@ public class ConflictServiceImpl implements ConflictService {
 
   @Inject
   private RoomRepository roomRepository;
+  @Inject
+  private ProfessorRepository professorRepository;
 
   private List<Room> rooms = new ArrayList<>();
+  private List<Professor> professors = new ArrayList<>();
+  private List<CourseSection> courseSections = new ArrayList<>();
   private List<MeetingDay> meetingDays = new ArrayList<>();
+  private List<MeetingDay> professorMeetingDays = new ArrayList<>();
 
   private static final Logger log = LoggerFactory.getLogger(
-      ConflictServiceImpl.class
+          ConflictServiceImpl.class
   );
 
   @Override
@@ -48,22 +56,65 @@ public class ConflictServiceImpl implements ConflictService {
           if (!Objects.equals(meetingDay.getId(), meetingDayToCompare.getId())) {
 
             if ((meetingDayToCompare.getDay().equals(meetingDay.getDay()))
-                && (meetingDayToCompare.getStartTime().before(meetingDay.getEndTime()))
-                && (meetingDayToCompare.getEndTime().after(meetingDay.getStartTime()))) {
+                    && (meetingDayToCompare.getStartTime().before(meetingDay.getEndTime()))
+                    && (meetingDayToCompare.getEndTime().after(meetingDay.getStartTime()))) {
 
               // Clear meeting day list to avoid stack overflow on client
               meetingDay.getCourseSection().setMeetingDayList(null);
 
               // Build conflict object and add it to conflicts collection
               conflicts.add(new Conflict(
-                  meetingDay.getCourseSection(),
-                  "Conflicts with line #: "
-                  + meetingDayToCompare.getCourseSection().getId()
+                      meetingDay.getCourseSection(),
+                      "Conflicts with line #: "
+                      + meetingDayToCompare.getCourseSection().getId()
+                      + " Room"
               ));
             }
           }
         }
       }
+    }
+
+    professors = professorRepository.findAllWithCourseSections();
+
+    for (Professor professor : professors) {
+
+      courseSections.clear();
+      professorMeetingDays.clear();
+      courseSections = professor.getCourseSectionList();
+
+      for (CourseSection courseSection : courseSections) {
+        meetingDays = courseSection.getMeetingDayList();
+        
+        for(MeetingDay md : meetingDays) {
+          professorMeetingDays.add(md);
+        }
+      }
+        
+        for (MeetingDay meetingDay : professorMeetingDays) {
+
+          for (MeetingDay meetingDayToCompare : professorMeetingDays) {
+
+            if (!Objects.equals(meetingDay.getId(), meetingDayToCompare.getId())) {
+
+              if ((meetingDayToCompare.getDay().equals(meetingDay.getDay()))
+                      && (meetingDayToCompare.getStartTime().before(meetingDay.getEndTime()))
+                      && (meetingDayToCompare.getEndTime().after(meetingDay.getStartTime()))) {
+
+                // Clear meeting day list to avoid stack overflow on client
+                //meetingDay.getCourseSection().setMeetingDayList(null);
+
+                // Build conflict object and add it to conflicts collection
+                conflicts.add(new Conflict(
+                        meetingDay.getCourseSection(),
+                        "Conflicts with line #: "
+                        + meetingDayToCompare.getCourseSection().getId()
+                        + " Professor"
+                ));
+              }
+            }
+          }
+        }
     }
 
     return new ArrayList<>(conflicts);
